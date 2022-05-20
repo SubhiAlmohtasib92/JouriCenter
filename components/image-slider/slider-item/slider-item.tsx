17dfd03116c13d2a9993d5666f144fcec2926ec7
SubhiAlmohtasib92 <subhi.almohtasib@gmail.com>
2022-05-20 18:15:29 +0300

    fixed the margin issue

--- components/image-slider/slider-item/slider-item.tsx
+++ components/image-slider/slider-item/slider-item.tsx
@@ -1,40 +1,42 @@
-import { View, Image, Text, Dimensions, Platform } from 'react-native';
+import { View, Image, Text, Dimensions, Platform } from "react-native";
 
-const { width, height } = Dimensions.get('screen');
+const { width, height } = Dimensions.get("screen");
 
 interface IProps {
-    imageURL: string;
+  imageURL: string;
 }
 
 const SliderItem = (props: IProps) => {
-    console.log('width', width);
-    
-    return (
-        <View style={{
-            justifyContent: 'center',
-            flex: 1,
-            height: '100%',
-            width: width - 20,
-            borderRadius: 10,
-            alignItems: 'center',
-            shadowColor: '#000',
-            shadowOffset: { width: 0.5, height: 0.5 },
-            shadowOpacity: 0.5,
-            shadowRadius: 3,
-            elevation: 5,
-            padding: 10,
-            backgroundColor: 'red',
-            marginHorizontal: 5,
-            marginLeft:10,
-        }} >
-            <Image style={{
-                width: width - 20,
-                height: 200,
-                borderRadius: 10
+  console.log("width", width);
 
-            }} source={{ uri: props.imageURL }} key={props.imageURL} />
-        </View>
-    )
+  return (
+    <View
+      style={{
+        justifyContent: "center",
+        flex: 1,
+        height: "100%",
+        width: "100%",
+        borderRadius: 10,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: { width: 0.5, height: 0.5 },
+        shadowOpacity: 0.5,
+        shadowRadius: 3,
+        elevation: 5,
+        paddingHorizontal: 10,
+      }}
+    >
+      <Image
+        style={{
+          width: width - 40,
+          height: 200,
+          borderRadius: 10,
+        }}
+        source={{ uri: props.imageURL }}
+        key={props.imageURL}
+      />
+    </View>
+  );
 };
 
-export default SliderItem; 
+export default SliderItem;
